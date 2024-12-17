dc675ea68c56af5dfbb979cda0a2cd3b28f31b2e
Vinícius Santos <santos.viniciusmt@gmail.com>
2024-12-17 16:44:36 -0300

    feat: no_ssl

--- certbot/renew_certificates.sh
+++ certbot/renew_certificates.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-# Tentar renovar os certificados
-certbot renew --quiet
+# Diretório do webroot usado pelo Certbot
+WEBROOT="/var/www/certbot"
 
-# Se a renovação for bem-sucedida, reiniciar o Nginx ou o servidor desejado
+# Tentar renovar os certificados
+certbot renew --webroot -w $WEBROOT --quiet
+
+# Verificar se a renovação foi bem-sucedida
 if [ $? -eq 0 ]; then
-    echo "Certificados renovados com sucesso. Reiniciando o Nginx..."
-    nginx -s reload
+    echo "$(date): Certificados renovados com sucesso." >> /var/log/certbot.log
+
+    # Verificar se o container do Nginx está em execução
+    if [ "$(docker ps -q -f name=webserver-nginx)" ]; then
+        echo "$(date): Reiniciando o Nginx..." >> /var/log/certbot.log
+        docker exec webserver-nginx nginx -s reload
+    else
+        echo "$(date): O container do Nginx não está em execução. Verifique!" >> /var/log/certbot.log
+    fi
 else
-    echo "Falha na renovação dos certificados."
+    echo "$(date): Falha na renovação dos certificados. Verifique os logs do Certbot!" >> /var/log/certbot.log
 fi
