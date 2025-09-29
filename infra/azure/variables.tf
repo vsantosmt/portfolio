41bdfde9daed58ffc55702df5d358fa626b7d3ff
SANTOS <v.santos@dxc.com>
2025-09-29 14:37:15 -0300

    Commit limpo sem arquivos .terraform grandes

---
+++ infra/azure/variables.tf
@@ -0,0 +1,14 @@
+variable "suffix" {
+  default = "portfolio"
+}
+
+variable "vm_name" {
+  default = "portfolio"
+}
+variable "location" {
+  default = "West Europe"
+}
+variable "vm_count" {
+  description = "Number of AVD machines to deploy"
+  default     = 1
+}
